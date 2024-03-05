d6a4f64de541a8a87949d6bb3815132d859d9ab6
Swarle <bogdanvalman@gmail.com>
2024-03-05 18:03:46 +0200

    Added timer

--- src/js/main.js
+++ src/js/main.js
@@ -3,16 +3,19 @@ import modals from './modules/modals';
 import tabs from './modules/tabs';
 import forms from './modules/forms'
 import changeModalState from "./modules/changeModalState";
+import timer from "./modules/timer";
 
 window.addEventListener("DOMContentLoaded", () => {
 
     const modalState = {};
 
     changeModalState(modalState);
+    const deadline = '2024-04-01';
 
     modals();
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
     tabs('.balcon_icons','.balcon_icons_img','.big_img > img','do_image_more','inline');
     forms(modalState);
+    timer('.container1', deadline);
 });
